c3b376442993f383403c1fb640f1b5a77a6ed036
Madeleine Heidl <made.in.beise@googlemail.com>
2023-04-17 13:26:40 +0200

    changes in translation of protein, for more rotation space
    Signed-off-by: Madeleine Heidl <made.in.beise@googlemail.com>

--- src/dockings/scrap4.jl
+++ src/dockings/scrap4.jl
@@ -1,17 +1,34 @@
 using BiochemicalAlgorithms
 
-include("load_trans_pdb.jl")
+protein = load_pdb("2ptc_protein.pdb")
 
-protein_B = load_and_trans_pdb("2ptc_ligand.pdb")
+# translate protein in positive space
+println("Translate protein in positive space...")
+# extract room coordinates of atoms of the protein
+atoms_in_space = protein.atoms.r
+# initalize vectors for storing x y z coordinates seperately
+X = Vector{Float32}()
+Y = Vector{Float32}()
+Z = Vector{Float32}()
 
-for i in 1:2
-    protein_B = load_and_trans_pdb("2ptc_ligand.pdb")
-    display(protein_B)
-
-    t = Vector3{Float32}(0,0,0)
-    R = Matrix3{Float32}([10 10 10; 10 10 10; 10 10 10])
-
-    rotation = RigidTransform(R,t)
-
-    rigid_transform!(protein_B, rotation)
+# fill vectors with coordinates
+for i in atoms_in_space
+    push!(X, i[1])
+    push!(Y, i[2])
+    push!(Z, i[3])
 end
+
+# calculate x y z min for translation (max maybe needed one time)
+min_x = minimum(X)
+max_x = maximum(X)
+min_y = minimum(Y)
+max_y = maximum(Y)
+min_z = minimum(Z)
+max_z = maximum(Z)
+
+DIA = Vector{Float32}()
+push!(DIA,abs(min_x-max_x)/2)
+push!(DIA,abs(min_y-max_y)/2)
+push!(DIA,abs(min_z-max_z)/2)
+
+maximum(DIA)
