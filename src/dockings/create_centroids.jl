42022a8973cd35d3b3bbfde1e18a8e0ac762fc8c
Madeleine Heidl <made.in.beise@googlemail.com>
2023-04-19 17:20:14 +0200

    outsoucing of some functions, new idea for setting colored cells
    Signed-off-by: Madeleine Heidl <made.in.beise@googlemail.com>

---
+++ src/dockings/create_centroids.jl
@@ -0,0 +1,23 @@
+using BiochemicalAlgorithms
+using Meshes
+
+function create_centroids(n, spfactor)
+    println("Build grid...")
+    # grid properties
+    lower_left = (0,0,0)
+    N = n
+    spcing_factor = spfactor
+    upper_right = (N,N,N) 
+    spcing = (N*spcing_factor, N*spcing_factor, N*spcing_factor)
+
+    # create 3D grid with N/N*spcing_factor spacing in each dimension, origin at (0,0,0)
+    grid = Meshes.CartesianGrid(lower_left, upper_right, dims=spcing)
+    # centroid of each cell
+    println("Build centroids...")
+    centroids = Meshes.centroid.(grid)
+    println(length(centroids))
+    # reshape centroids 1D -> 3D 
+    centroids = reshape([centroids...], N, N, N)
+
+    return centroids
+end
